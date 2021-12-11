d33ee47d853f43c21f1f62e5166255994ebf2ef8
raja09876 <you@example.com>
2021-12-11 22:53:03 +0530

    first commint

---
+++ src/Page/LoginPage.java
@@ -0,0 +1,17 @@
+package Page;
+
+public class LoginPage {
+
+	public void login() {
+		
+		System.out.println(" hello login");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ src/Test/LoginPageTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import Page.LoginPage;
+
+public class LoginPageTest {
+
+	
+	public static void main(String[] args) {
+		
+		LoginPage login = new LoginPage();
+		
+		login.login();
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
